1701b7ea209f2f9f40f816a6babe3164655630ac
Mohamed Ayman <mousa2692004@gmail.com>
2026-03-29 22:27:20 +0200

    setup hosting and deployment with Firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:portfoilo/core/my_bloc_observer.dart';
 import 'package:portfoilo/core/utils/app_routes.dart';
 import 'package:portfoilo/core/utils/app_theme.dart';
+import 'package:portfoilo/feature/firebase_services/firebase_options.dart';
 import 'package:portfoilo/feature/portfolio/cubit/portfolio_cubit.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   Bloc.observer = MyBlocObserver();
   runApp(
     BlocProvider(create: (context) => PortfolioCubit(), child: const MyApp()),
